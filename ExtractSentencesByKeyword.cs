9c0c37ac4c583a9e8c5818e2e96b5102caa90d32
Ivan <ivan.naumov92@yahoo.com>
2018-06-24 17:35:36 +0300

    Import

---
+++ ExtractSentencesByKeyword.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ExtractSentencesByKeyword
+{
+    class ExtractSentencesByKeyword
+    {
+        static void Main(string[] args)
+        {
+            var word = Console.ReadLine().ToLower();
+            var input = Console.ReadLine().Split('!', '.', '?');
+            var pattern = $@"\b([{word}]+)\b";
+            var regex = new Regex(pattern);
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var matches = regex.Matches(input[i]);
+                foreach (Match match in matches)
+                {
+                    if (match.Success == true)
+                    {
+                        Console.WriteLine(input[i].Trim());
+                    }
+                }
+
+            }
+
+
+        }
+    }
+}
